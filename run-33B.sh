c774e8f4ceb8992e35740126cf0ae132c51bf933
lambda7xx <lambda7xx@gmail.com>
2023-07-26 06:35:18 +0000

    add opt 33B

--- run-33B.sh
+++ run-33B.sh
@@ -1,26 +1,26 @@
-gpu_batch_sizes=(1 2 4 8 16 32)
-gpu_weight_percent=0
-cpu_weight_percent=100 
+gpu_batch_sizes=(16 32)
+# gpu_weight_percent=0
+# cpu_weight_percent=100 
 
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
-
-
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
 
 
-gpu_weight_percent=10
-cpu_weight_percent=90 
-
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
 
 
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
+# gpu_weight_percent=10
+# cpu_weight_percent=90 
+
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+
+
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
 
 gpu_weight_percent=20
 cpu_weight_percent=80 
@@ -33,59 +33,59 @@ do
     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
 done
 
-gpu_weight_percent=30
-cpu_weight_percent=70 
+# gpu_weight_percent=30
+# cpu_weight_percent=70 
 
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
-
-
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
-
-gpu_weight_percent=40
-cpu_weight_percent=60 
-
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
 
 
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
 
-gpu_weight_percent=50
-cpu_weight_percent=50 
+# gpu_weight_percent=40
+# cpu_weight_percent=60 
 
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
-
-
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
-
-gpu_weight_percent=60
-cpu_weight_percent=40 
-
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
 
 
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
+
+# gpu_weight_percent=50
+# cpu_weight_percent=50 
+
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+
+
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
+
+# gpu_weight_percent=60
+# cpu_weight_percent=40 
+
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+
+
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
 
 
 
-gpu_weight_percent=70
-cpu_weight_percent=30 
+# gpu_weight_percent=70
+# cpu_weight_percent=30 
 
-for gpu_batch_size in "${gpu_batch_sizes[@]}"
-do 
-    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+# for gpu_batch_size in "${gpu_batch_sizes[@]}"
+# do 
+#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
 
 
-    #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-done
+#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+# done
