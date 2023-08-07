d50ac78344d62b3a434d1980f390e22f058cdd0f
Ubuntu <ubuntu@ip-172-31-5-120.us-east-2.compute.internal>
2023-08-07 08:31:17 +0000

    add opt-30B

--- run-33B.sh
+++ run-33B.sh
@@ -1,15 +1,18 @@
-gpu_batch_sizes=(16 32)
-# gpu_weight_percent=0
-# cpu_weight_percent=100 
+gpu_batch_sizes=(1 2 4 8 16 32)
+# gpu_batch_sizes=(1)
+gpu_weight_percent=0
+cpu_weight_percent=100 
 
-# for gpu_batch_size in "${gpu_batch_sizes[@]}"
-# do 
-#     python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent.log 2>&1 
+gen_len=128
+
+time_current=0807
+for gpu_batch_size in "${gpu_batch_sizes[@]}"
+do 
+    python3 -m flexgen.flex_opt --model facebook/opt-30b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent-gen_len-$gen_len-time-$time_current.log 2>&1 
 
 
-#     #code  opt_30B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
-# done
-
+    #code  opt_6.7B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
+done
 
 # gpu_weight_percent=10
 # cpu_weight_percent=90 
--- run-opt-13b.sh
+++ run-opt-13b.sh
@@ -1,4 +1,4 @@
-gpu_batch_sizes=(1 2 4 8 16)
+gpu_batch_sizes=(1 2 4 8 16 32)
 # gpu_batch_sizes=(1)
 gpu_weight_percent=0
 cpu_weight_percent=100 
@@ -8,7 +8,7 @@ gen_len=128
 time_current=0807
 for gpu_batch_size in "${gpu_batch_sizes[@]}"
 do 
-    python3 -m flexgen.flex_opt --model facebook/opt-6.7b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_6.7B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent-gen_len-$gen_len-time-$time_current.log 2>&1 
+    python3 -m flexgen.flex_opt --model facebook/opt-13b --percent  $gpu_weight_percent $cpu_weight_percent 100 0 100 0 --gpu_batch_size $gpu_batch_size --overlap > opt_13B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_percent-cpu_weight_percent-$cpu_weight_percent-gen_len-$gen_len-time-$time_current.log 2>&1 
 
 
     #code  opt_6.7B-gpu_batch_size-$gpu_batch_size-gpu_weight_percent-$gpu_weight_ercent-cpu_weight_percent-$cpu_weight_percent.log
